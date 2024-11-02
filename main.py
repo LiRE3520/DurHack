8848704af2e2cc0da7c112fadde394fff19f0a8e
Patrick-Burgess <patrickeburgess05@gmail.com>
2024-11-02 17:40:23 +0000

    Added TheTeam

--- main.py
+++ main.py
@@ -11,3 +11,7 @@ def hello():
 @app.route('/routePlanner')
 def routePlanner():
     return render_template('routePlanner.html')
+
+@app.route('/team')
+def theTeam():
+    return render_template('TheTeam.html')
